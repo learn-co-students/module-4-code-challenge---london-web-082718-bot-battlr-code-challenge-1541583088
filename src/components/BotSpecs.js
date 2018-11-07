d84e044df3c2d72b5da1f0c30761d716c3aad194
Billiesnyc <billiesnyc@gmail.com>
2018-11-07 11:03:48 +0000

    challenge completed

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -60,21 +60,15 @@ const BotSpecs = props => {
             </div>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log('connect this to a function that shows all bots')
-              }
+              onClick={() => props.deselectBot() }
             >
               Go Back
             </button>
             <button
               className="ui button fluid"
-              onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
-              }
+              onClick={() => props.enlistBot(bot)}
             >
-              Enlist
+              {props.enlisted ? 'De-Enlist' : 'Enlist'}
             </button>
           </div>
         </div>
